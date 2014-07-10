30571df415a0c6d5eee51682e943e1466fd608ce
Matthew Hollingworth <mdholling@gmail.com>
2014-07-10 15:21:31 +1000

    add --days option for batching

--- au-snow.rb
+++ au-snow.rb
@@ -5,6 +5,7 @@
 date = Date.today
 satellite = "terra"
 FlickRaw.api_key = "cef9b7cd3df5f75351acd80f60ff5b47"
+days = nil
 
 OptionParser.new("usage: au-snow.rb [options]") do |options|
   options.on "--date DATE", "date of satellite imagery" do |value|
@@ -13,8 +14,6 @@ OptionParser.new("usage: au-snow.rb [options]") do |options|
     rescue ArgumentError
       options.abort "bad date"
     end
-    options.abort "date is before 2004-10-23" unless date >= Date.new(2004, 10, 23)
-    options.abort "date is in the future"     unless date <= Date.today
   end
   options.on "--satellite [terra|aqua]", "MODIS satellite", %w[terra aqua] do |value|
     satellite = value
@@ -31,41 +30,63 @@ OptionParser.new("usage: au-snow.rb [options]") do |options|
   options.on "--access-secret SECRET", "flickr access secret" do |value|
     flickr.access_secret = value
   end
+  options.on "--days DAYS", "consecutive days to download" do |value|
+    days = value.to_i
+  end
 end.parse!
 
-Dir.mktmpdir do |dir|
-  dir = Pathname.new(dir)
-  specifiers = "%d%03d.%s.250m" % [ date.year, date.yday, satellite ]
-  img, jgw, txt = [ %w[jpg jgw txt], %w[wb w w] ].transpose.map do |ext, flags|
-    path = dir + "#{specifiers}.#{ext}"
-    uri = URI.parse("http://lance-modis.eosdis.nasa.gov/imagery/subsets/?project=fas&subset=FAS_SEAustralia3.#{specifiers}.#{ext}")
-    path.open(flags) { |file| file << uri.read }
-    path
-  end
-  abort("data not available for that date") if txt.read[/error/i]
-  time = txt.read.each_line.grep(/^(\d\d):(\d\d) UTC/) do
-    Time.utc(date.year, date.month, date.day, $1.to_i, $2.to_i)
-  end.last.getlocal("+10:00")
+UnavailableError = Class.new(StandardError);
 
-  [
-    [ "nsw", "147.768908 -35.414950 149.112564 -36.929548" ],
-    [ "vic", "146.141080 -36.632428 147.541791 -37.918267" ],
-  ].each do |state, window|
-    title = "#{state}-#{date}-#{satellite}"
-    set_title = "au-snow-#{state}"
-    tif = dir + "#{title}.tif"
-    jpg = dir + "#{title}.jpg"
-    %x[gdal_translate -projwin #{window} "#{img}" "#{tif}"]
-    %x[convert -quiet "#{tif}" "#{jpg}"]
-    flickr.upload_photo(jpg, :title => title).tap do |id|
-      flickr.photos.setDates(:photo_id => id, :date_taken => time.strftime("%F %T"))
-      flickr.photos.setTags(:photo_id => id, :tags => "au-snow:year=#{date.year} au-snow:state=#{state} au-snow:satellite=#{satellite} au-snow:yday=#{date.yday}")
-      flickr.photos.setPerms(:photo_id => id, :is_public => 1, :is_friend => 0, :is_family => 1, :perm_comment => 0, :perm_addmeta => 0)
-      flickr.photosets.getList.find do |set|
-        set.title == set_title
-      end.tap do |set|
-        set ? flickr.photosets.addPhoto(:photoset_id => set.id, :photo_id => id) : flickr.photosets.create(:title => set_title, :primary_photo_id => id)
-      end
-    end unless flickr.photos.search(:user_id => "me", :text => title).any?
+def get(date, satellite)
+  Dir.mktmpdir do |dir|
+    dir = Pathname.new(dir)
+    specifiers = "%d%03d.%s.250m" % [ date.year, date.yday, satellite ]
+    img, jgw, txt = [ %w[jpg jgw txt], %w[wb w w] ].transpose.map do |ext, flags|
+      path = dir + "#{specifiers}.#{ext}"
+      uri = URI.parse("http://lance-modis.eosdis.nasa.gov/imagery/subsets/?project=fas&subset=FAS_SEAustralia3.#{specifiers}.#{ext}")
+      path.open(flags) { |file| file << uri.read }
+      path
+    end
+    raise UnavailableError.new("#{date} #{satellite}: data not available") if txt.read[/error/i]
+    time = txt.read.each_line.grep(/^(\d\d):(\d\d) UTC/) do
+      Time.utc(date.year, date.month, date.day, $1.to_i, $2.to_i)
+    end.last.getlocal("+10:00")
+
+    [
+      [ "nsw", "147.768908 -35.414950 149.112564 -36.929548" ],
+      [ "vic", "146.141080 -36.632428 147.541791 -37.918267" ],
+    ].each do |state, window|
+      title = "#{state}-#{date}-#{satellite}"
+      set_title = "au-snow-#{state}"
+      tif = dir + "#{title}.tif"
+      jpg = dir + "#{title}.jpg"
+      %x[gdal_translate -projwin #{window} "#{img}" "#{tif}"]
+      %x[convert -quiet "#{tif}" "#{jpg}"]
+      flickr.upload_photo(jpg, :title => title).tap do |id|
+        flickr.photos.setDates(:photo_id => id, :date_taken => time.strftime("%F %T"))
+        flickr.photos.setTags(:photo_id => id, :tags => "au-snow:year=#{date.year} au-snow:state=#{state} au-snow:satellite=#{satellite} au-snow:yday=#{date.yday}")
+        flickr.photos.setPerms(:photo_id => id, :is_public => 1, :is_friend => 0, :is_family => 1, :perm_comment => 0, :perm_addmeta => 0)
+        flickr.photosets.getList.find do |set|
+          set.title == set_title
+        end.tap do |set|
+          set ? flickr.photosets.addPhoto(:photoset_id => set.id, :photo_id => id) : flickr.photosets.create(:title => set_title, :primary_photo_id => id)
+        end
+      end unless flickr.photos.search(:user_id => "me", :text => title).any?
+    end
   end
 end
+
+(0...days).map do |count|
+  date + count
+end.each do |date|
+  %w[terra aqua].each do |satellite|
+    begin
+      get(date, satellite)
+      STDOUT.puts"#{date} #{satellite}: downloaded"
+    rescue UnavailableError => e
+      STDERR.puts e.message;
+    end
+  end
+end if days
+
+get(date, satellite) if !days
