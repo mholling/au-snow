9fac684044bfc170d55bc1876c47670719a54a87
Matthew Hollingworth <mdholling@gmail.com>
2014-07-16 09:57:19 +1000

    fix typo in sort function

--- au-snow.js
+++ au-snow.js
@@ -89,7 +89,7 @@
         self.loading = true;
         return flickrSearch({ state: state, year: year}).success(function(data) {
           self.cache[state][year].photos = data.photos.photo.sort(function(photo1, photo2) {
-            return photo1.datetaken < photo2.datetaken ?  1 : photo1.datetaken > photo2.datetaken ? 1 : 0;
+            return photo1.datetaken < photo2.datetaken ? -1 : photo1.datetaken > photo2.datetaken ? 1 : 0;
           }).map(function(photo) {
             var parts = photo.datetaken.split(/[- :]/);
             var match = photo.machine_tags.match(/ausnow:satellite=(terra|aqua)/);
